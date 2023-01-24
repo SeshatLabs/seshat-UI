c10feb060f85dd286a7321af23124fd39408964f
Patrick He <patrick.zebing.he@gmail.com>
2023-01-23 21:11:13 -0500

    Fix duplicate contracts and add tests

--- data-engine/ETL/dao/contract.dao.js
+++ data-engine/ETL/dao/contract.dao.js
@@ -1,17 +1,21 @@
 import Contract from '../entities/contract.entity'
 
 export async function createContract(contract) {
-    try {
-      const existingContract = await Contract.findOne({ SourceCode: contract.SourceCode });
-      if (existingContract) {
-        console.log('Contract with provided Address already exists, bypass...');
-        return null;
-      } else {
-        const finalContract = await Contract.create(contract);
-        console.log('Contract created successfully');
-        return finalContract;
-      }
-    } catch (error) {
-      console.error(error);
-    }
+  try {
+    const finalContract = await Contract.create(contract);
+    console.log('Contract created successfully');
+    return finalContract;
+  } catch (error) {
+    //console.error(error);
   }
+}
+
+export async function checkContractExists(contractAddress) {
+  try {
+    const existingContract = await Contract.findOne({ contractAddress: contractAddress });
+    if (existingContract) return true;
+    return false;
+  } catch (error) {
+    console.error(error);
+  }
+}
