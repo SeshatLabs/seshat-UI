69d4e44cb8104a3d970d1b684b8bb735fced71cd
JustinSo1 <sojustin28@gmail.com>
2023-05-01 15:58:09 -0400

    Modify link to prod landing page in Header to redirect to local landing page instead

--- components/Header/index.js
+++ components/Header/index.js
@@ -63,7 +63,7 @@ import {
           justify={'space-between'}
         >
           <Box>
-          <Link href="https://www.seshatlabs.xyz" isExternal>
+          <Link href="/">
             <Text fontSize="lg" fontWeight="bold" paddingLeft={{base: '0', md: '100px'}}> 
               Seshat Labs
             </Text>
