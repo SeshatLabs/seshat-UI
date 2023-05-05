5a553e4cde4fef7144b61fc2fc83cd01322dfe27
JustinSo1 <sojustin28@gmail.com>
2023-05-05 01:57:18 -0400

    Add auth0 to build campagin page. Only allow logged in users to utilize the build campagin feature

--- components/Ads/Builder.js
+++ components/Ads/Builder.js
@@ -1,6 +1,8 @@
-import { Text, Box, Select, FormLabel, FormControl, Textarea, Input, Button, TableContainer, Thead, Td, Tr, Th, Tbody, Table, Accordion, AccordionItem, AccordionButton, AccordionPanel } from "@chakra-ui/react"
-import styles from './builder.module.css'
+import { Text, Box, Select, FormLabel, Container, FormControl, Textarea, Input, Button, TableContainer, Thead, Td, Tr, Th, Tbody, Table, Accordion, AccordionItem, AccordionButton, AccordionPanel } from "@chakra-ui/react";
+import styles from './builder.module.css';
 import { useState, useEffect, useRef } from 'react'
+import { useRouter } from 'next/router';
+import { useUser } from '@auth0/nextjs-auth0/client';
 import axios from "axios";
 
 const CURRENT_ADVERTISER = 'test';
@@ -15,6 +17,8 @@ export default function Builder({ builderSelected }) {
     const [budget, setBudget] = useState('');
     const inputRef = useRef();
     const [files, setFiles] = useState([]);
+    const { user, error, isLoading } = useUser();
+    const router = useRouter();
 
     function handleEntryOnClick(campaign) {
         if (selectedCampaign && campaign._id == selectedCampaign._id) {
@@ -86,140 +90,163 @@ export default function Builder({ builderSelected }) {
         axios.post('/api/create_campaign', formData)
     }
 
-    return <Box className={`${styles.builder} ${builderSelected ? '' : styles.noDisplay}`}>
-        <Box className={styles.row}>
-            <Box className={styles.section}>
-                <Text fontSize='3xl'>1. Campaign</Text>
-                <FormControl>
-                    <Box className={styles.row}>
-                        <Box className={styles.section}>
-                            <Box className={styles.textin}>
-                                <FormLabel>Mode</FormLabel>
-                                <Select onChange={handleModeChange}>
-                                    <option value='simple'>Simple</option>
-                                    <option value='airdrop'>Airdrop</option>
-                                </Select>
-                            </Box>
-                        </Box>
-                        <Box className={styles.section}>
-                            <Box className={styles.textin}>
-                                <FormLabel>Title</FormLabel>
-                                <Input placeholder='Title' onChange={handleTitleChange} className={styles.textin} />
-                            </Box>
-                        </Box>
-                    </Box>
-                    <Box className={styles.row}>
-                        <Box className={styles.section}>
-                            <FormLabel>Description</FormLabel>
-                            <Box className={styles.textin}>
-                                <Textarea className={styles.textin} placeholder='I want to run a campaign on my...' onChange={handleDescriptionChange} />
-                                <input
-                                    type="file"
-                                    ref={inputRef}
-                                    style={{ display: "none" }}
-                                    multiple={true}
-                                    onChange={handleOnFileChange}
-                                />
-                            </Box>
-                        </Box>
-                    </Box>
-                </FormControl>
+    if (isLoading) {
+        return <div>Loading...</div>;
+    }
+
+    if (error) {
+        return <div>Error: {error.message === undefined ? "Unauthorized usage of auth0" : error.message}</div>;
+    };
+
+    if (user) {
+        return (
+            <Box className={`${styles.builder} ${builderSelected ? '' : styles.noDisplay}`}>
                 <Box className={styles.row}>
-
-                    <Box className={styles.bottom}>
-                        <Button variant='outline' colorScheme='purple' onClick={() => { inputRef.current.click() }}>Upload Media</Button>
-                    </Box>
-
-                </Box>
-
-            </Box>
-            <Box className={styles.section}>
-                <Box className={styles.row}>
-                    <Text fontSize='3xl'>2. Details</Text>
-                </Box>
-                <FormControl className={styles.makeflex}>
-
                     <Box className={styles.section}>
-                        <FormLabel>Duration</FormLabel>
-                        <Box className={styles.textin}>
-                            <Select className={styles.textin} onChange={handleDurationChange} >
-                                <option value='30'>30 Days</option>
-                                <option value='60'>60 Days</option>
-                            </Select>
+                        <Text fontSize='3xl'>1. Campaign</Text>
+                        <FormControl>
+                            <Box className={styles.row}>
+                                <Box className={styles.section}>
+                                    <Box className={styles.textin}>
+                                        <FormLabel>Mode</FormLabel>
+                                        <Select onChange={handleModeChange}>
+                                            <option value='simple'>Simple</option>
+                                            <option value='airdrop'>Airdrop</option>
+                                        </Select>
+                                    </Box>
+                                </Box>
+                                <Box className={styles.section}>
+                                    <Box className={styles.textin}>
+                                        <FormLabel>Title</FormLabel>
+                                        <Input placeholder='Title' onChange={handleTitleChange} className={styles.textin} />
+                                    </Box>
+                                </Box>
+                            </Box>
+                            <Box className={styles.row}>
+                                <Box className={styles.section}>
+                                    <FormLabel>Description</FormLabel>
+                                    <Box className={styles.textin}>
+                                        <Textarea className={styles.textin} placeholder='I want to run a campaign on my...' onChange={handleDescriptionChange} />
+                                        <input
+                                            type="file"
+                                            ref={inputRef}
+                                            style={{ display: "none" }}
+                                            multiple={true}
+                                            onChange={handleOnFileChange}
+                                        />
+                                    </Box>
+                                </Box>
+                            </Box>
+                        </FormControl>
+                        <Box className={styles.row}>
+
+                            <Box className={styles.bottom}>
+                                <Button variant='outline' colorScheme='purple' onClick={() => { inputRef.current.click() }}>Upload Media</Button>
+                            </Box>
+
                         </Box>
+
                     </Box>
                     <Box className={styles.section}>
-                        <FormLabel>Budget</FormLabel>
-                        <Box className={styles.textin}>
-                            <Input className={styles.textin} placeholder='$' onChange={handleBudgetChange} />
+                        <Box className={styles.row}>
+                            <Text fontSize='3xl'>2. Details</Text>
+                        </Box>
+                        <FormControl className={styles.makeflex}>
+
+                            <Box className={styles.section}>
+                                <FormLabel>Duration</FormLabel>
+                                <Box className={styles.textin}>
+                                    <Select className={styles.textin} onChange={handleDurationChange} >
+                                        <option value='30'>30 Days</option>
+                                        <option value='60'>60 Days</option>
+                                    </Select>
+                                </Box>
+                            </Box>
+                            <Box className={styles.section}>
+                                <FormLabel>Budget</FormLabel>
+                                <Box className={styles.textin}>
+                                    <Input className={styles.textin} placeholder='$' onChange={handleBudgetChange} />
+                                </Box>
+                            </Box>
+
+
+                        </FormControl>
+                        <Box className={`${styles.row} ${styles.fill}`}>
+                            <Box className={` ${styles.bottom}`}>
+                                <Button variant='outline' colorScheme='purple' onClick={handleCampaignOnClick}>Run Campaign</Button>
+                            </Box>
                         </Box>
                     </Box>
+                </Box>
+                <Box className={styles.row} my='16'>
+                    <Box className={`${styles.section} ${prevCampaigns ? '' : styles.hidden}`}>
+                        <TableContainer >
+                            <Table variant='simple'>
+                                <Thead>
+                                    <Tr>
+                                        <Th>Campaigns</Th>
+                                        <Th>Remaining</Th>
+                                        <Th>Description</Th>
+                                    </Tr>
+                                </Thead>
+                                {
+                                    prevCampaigns.map((campaign) => {
+                                        return <Tbody className={`${styles.tableEntry} ${selectedCampaign && campaign._id == selectedCampaign._id ? styles.selected : ''}`} key={campaign._id} onClick={() => { handleEntryOnClick(campaign); }}><Tr>
+                                            <Td>{campaign.title}</Td>
+                                            <Td>{campaign.timeRemaining} days</Td>
+                                            <Td>{campaign.description.slice(0, 10)}</Td>
+                                        </Tr></Tbody>
+                                    })
+                                }
+                            </Table>
+                        </TableContainer>
 
 
-                </FormControl>
-                <Box className={`${styles.row} ${styles.fill}`}>
-                    <Box className={` ${styles.bottom}`}>
-                        <Button variant='outline' colorScheme='purple' onClick={handleCampaignOnClick}>Run Campaign</Button>
+                    </Box>
+                    <Box className={`${styles.section} ${prevCampaigns ? '' : styles.hidden}`}>
+                        {selectedCampaign ?
+                            <Accordion allowMultiple>
+                                <AccordionItem>
+                                    <h2>
+                                        <AccordionButton>
+                                            <Box as="span" flex='1' textAlign='left'>
+                                                Content
+                                            </Box>
+                                        </AccordionButton>
+                                    </h2>
+                                    <AccordionPanel>
+                                        <img className={styles.preview} src={`data:image/png;base64,${Buffer.from(selectedCampaign.media).toString('base64')}`} />
+                                    </AccordionPanel>
+                                </AccordionItem>
+                                <AccordionItem>
+                                    <h2>
+                                        <AccordionButton>
+                                            <Box as="span" flex='1' textAlign='left'>
+                                                Details
+                                            </Box>
+                                        </AccordionButton>
+                                    </h2>
+                                    <AccordionPanel>
+                                        <Box>{selectedCampaign.timeRemaining} days remaining</Box>
+                                        <Box>${selectedCampaign.budget} left in your budget</Box>
+                                    </AccordionPanel>
+                                </AccordionItem>
+                            </Accordion>
+                            : <></>}
                     </Box>
                 </Box>
             </Box>
-        </Box>
-        <Box className={styles.row} my='16'>
-            <Box className={`${styles.section} ${prevCampaigns ? '' : styles.hidden}`}>
-                <TableContainer >
-                    <Table variant='simple'>
-                        <Thead>
-                            <Tr>
-                                <Th>Campaigns</Th>
-                                <Th>Remaining</Th>
-                                <Th>Description</Th>
-                            </Tr>
-                        </Thead>
-                        {
-                            prevCampaigns.map((campaign) => {
-                                return <Tbody className={`${styles.tableEntry} ${selectedCampaign && campaign._id == selectedCampaign._id ? styles.selected : ''}`} key={campaign._id} onClick={() => { handleEntryOnClick(campaign); }}><Tr>
-                                    <Td>{campaign.title}</Td>
-                                    <Td>{campaign.timeRemaining} days</Td>
-                                    <Td>{campaign.description.slice(0, 10)}</Td>
-                                </Tr></Tbody>
-                            })
-                        }
-                    </Table>
-                </TableContainer>
+        );
+    }
 
-
-            </Box>
-            <Box className={`${styles.section} ${prevCampaigns ? '' : styles.hidden}`}>
-                {selectedCampaign ?
-                    <Accordion allowMultiple>
-                        <AccordionItem>
-                            <h2>
-                                <AccordionButton>
-                                    <Box as="span" flex='1' textAlign='left'>
-                                        Content
-                                    </Box>
-                                </AccordionButton>
-                            </h2>
-                            <AccordionPanel>
-                                <img className={styles.preview} src={`data:image/png;base64,${Buffer.from(selectedCampaign.media).toString('base64')}`} />
-                            </AccordionPanel>
-                        </AccordionItem>
-                        <AccordionItem>
-                            <h2>
-                                <AccordionButton>
-                                    <Box as="span" flex='1' textAlign='left'>
-                                        Details
-                                    </Box>
-                                </AccordionButton>
-                            </h2>
-                            <AccordionPanel>
-                                <Box>{selectedCampaign.timeRemaining} days remaining</Box>
-                                <Box>${selectedCampaign.budget} left in your budget</Box>
-                            </AccordionPanel>
-                        </AccordionItem>
-                    </Accordion>
-                    : <></>}
-            </Box>
-        </Box>
-    </Box>
+    return (
+        <Container maxW="xl" mt={80} textAlign="center">
+            <Text fontSize="xl" mb={4}>
+                You are not logged in, please hit the continue to create a new account or login into your account.
+            </Text>
+            <Button colorScheme="blue" onClick={() => router.push('/api/auth/login')}>
+                Login
+            </Button>
+        </Container>
+    );
 }
