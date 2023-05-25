cca200065dcbab7d23bed0eb98d9573ff678f523
JustinSo1 <sojustin28@gmail.com>
2023-05-24 23:35:24 -0400

    Add PublisherDashboard to Publisher Zone in /ads

--- components/Ads/PublisherDashboard.js
+++ components/Ads/PublisherDashboard.js
@@ -1,41 +1,58 @@
-import { Box, Text, Tag } from "@chakra-ui/react";
+import { Box, Text, Center, Button, Flex, Spacer } from "@chakra-ui/react";
 import styles from "./builder.module.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 
 export default function PublisherDashboard({ builderSelected, user }) {
   const [revenue, setRevenue] = useState(0);
 
   useEffect(() => {
+    let gotRevenue = false;
     async function getRevenue(userID) {
       const user = await axios.get("/api/get_user", {
         params: {
           userID: userID,
         },
       });
-      return user.data.revenue;
+      if (!user || !user.data.data) {
+        throw "Could not find user";
+      }
+      const revenue = user.data.data.revenue;
+      if (!gotRevenue) {
+        setRevenue(revenue);
+      }
     }
-    const revenue = getRevenue(user.userID);
-    setRevenue(revenue);
-  }, []);
+    getRevenue(user.sub).catch(console.error);
+    return () => (gotRevenue = true);
+  }, [revenue]);
+
+  const handleWithdraw = (e) => {};
 
   return (
     <Box
       className={`${styles.builder} ${builderSelected ? styles.noDisplay : ""}`}
     >
-      <Text fontSize="lg">
-        <Tag color="black" fontWeight="bold" fontSize="lg">
-          Your Publisher/DApp Developer API Key:{" "}
-        </Tag>
-        {user.seshat_API_keys.publisherAPIKey}
+      <Text as="b" fontSize="lg">
+        Your Publisher/DApp Developer API Key
       </Text>
+      <Center h="100px">
+        <Text fontSize="md">{user.seshat_API_keys.publisherAPIKey}</Text>
+      </Center>
 
-      <Text fontSize="lg">
-        <Tag color="black" fontWeight="bold" fontSize="lg">
-          Revenue:{" "}
-        </Tag>
-        {revenue}
+      <br />
+
+      <Text as="b" fontSize="lg">
+        Revenue
       </Text>
+      <Center h="100px">
+        <Text fontSize="md">{revenue}</Text>
+      </Center>
+      <Flex>
+        <Spacer />
+        <Button variant="outline" colorScheme="blue" onClick={handleWithdraw}>
+          Withdraw
+        </Button>
+      </Flex>
     </Box>
   );
 }
